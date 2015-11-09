4b78c2729c0b2640d18dc1d8c80cce91a0b11731
max33nau <mtj29@nau.edu>
2015-11-09 14:16:45 -0800

    Grunt Work

--- js/votetracker.js
+++ js/votetracker.js
@@ -1,1 +1,31 @@
 //https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js
+var defaultInput = [["Asparagus",   "asparagus-2039__180.jpg"],
+                    ["Avocado",     "avocado-161822__180.png"],
+                    ["Cherries",    "bing-cherries-805416__180.jpg"],
+                    ["Turkey",      "blueMarinatedTurkey.jpg"],
+                    ["Condiments",  "condimentSprays.jpg"],
+                    ["CornDogs",    "cornDogs.jpg"],
+                    ["Candles",     "cranberryCandles.jpg"],
+                    ["CreamPuffs",  "cream-puffs-427181__180.jpg"],
+                    ["Egg",         "egg-157224__180.png"],
+                    ["Shrimp",      "frozenShrimp.jpg"],
+                    ["Casserole",   "frozenWhatAreThese.jpg"],
+                    ["Garlic",      "garlic-84691__180.jpg"],
+                    ["Soda",        "heatedSoda.jpg"],
+                    ["Jello",       "jelloMayoTurkey.jpg"],
+                    ["Fruit",       "kiwifruit-400143__180.jpg"],
+                    ["Mushrooms",   "mushrooms-756406__180.jpg"],
+                    ["Pasta",       "pasta-503952__180.jpg"],
+                    ["Tomato",      "tomatoes-320860__180.jpg"],
+                    ["Soup",        "whipcreamSoup.jpg"],
+                    ["Pepper",      "yellowpepper-22111__180.jpg"]];
+var VOTE_MODULE = (function() {
+
+  var my = {};
+  my.anchorNode = document.getElementById( "VoteAnchor" );
+  my.imageArray = [];
+
+
+
+
+} )();
