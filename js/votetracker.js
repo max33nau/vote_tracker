357861c835f929079f4d9e62cdcd34c3a02a2d42
max33nau <mtj29@nau.edu>
2015-11-11 15:33:55 -0800

    whitespace

--- js/votetracker.js
+++ js/votetracker.js
@@ -1,10 +1,3 @@
-/* * * * * * * * * * * * * * * *
- * VOTE TRACKER                *
- * Created by   John Hearn     *
- *              Doug Popadince *
- *              Max Jacobson   *
- * CF201        Fall 2015      *
- * * * * * * * * * * * * * * * */
 
 var defaultInput = [ [ "Asparagus",   "asparagus-2039__180.jpg" ],
                      [ "Avocado",     "avocado-161822__180.png" ],
@@ -26,11 +19,6 @@ var defaultInput = [ [ "Asparagus",   "asparagus-2039__180.jpg" ],
                      [ "Tomato",      "tomatoes-320860__180.jpg" ],
                      [ "Soup",        "whipcreamSoup.jpg" ],
                      [ "Pepper",      "yellowpepper-22111__180.jpg" ] ];
-
-
-    /* * * * * * * * * * * * * * * * * * *
-     * * * * * * CONSTRUCTORS  * * * * * *
-     * * * * * * * * * * * * * * * * * * */
 
 function Picture( name, fileName ) {
     this.name = name;
@@ -63,60 +51,60 @@ var VOTE_MODULE = (function() {
      * * * * * * * * * * * * * * * * * * */
 
     my.pictures.init = function( initData ) {
-    	for ( var ii=0; ii < initData.length; ii++ ) {
-    	    var picture = new Picture( initData[ii][0],
+      for ( var ii=0; ii < initData.length; ii++ ) {
+          var picture = new Picture( initData[ii][0],
                                        initData[ii][1] );
-    	    my.pictures.push( picture );
-	    // Add random votes to test per HW12
+          my.pictures.push( picture );
+      // Add random votes to test per HW12
             my.voteTotal += picture.vote;
             // console.log("show vote total: " + my.voteTotal);
-    	}
+      }
     }
 
     my.generateRandomIndices = function( length ) {
-	var tempIndexArray = [ ];
-	for ( var ii = 0; ii < length; ii++ ) {
-	    tempIndexArray.push( ii );
-	}
+  var tempIndexArray = [ ];
+  for ( var ii = 0; ii < length; ii++ ) {
+      tempIndexArray.push( ii );
+  }
 
-	// If there is an odd number of candidates we may have one index
-	// left in our array, so we empty it.
-	my.randomizedIndices = [ ];
+  // If there is an odd number of candidates we may have one index
+  // left in our array, so we empty it.
+  my.randomizedIndices = [ ];
 
-	while ( tempIndexArray.length > 0 ) {
-	    var index = Math.floor( Math.random() * tempIndexArray.length );
-	    // Splice() deletes an element from an array and returns it as a single
-	    // element array. We dereference it and push onto our array of indices.
-	    my.randomizedIndices.push( tempIndexArray.splice( index , 1 ) );
-	}
+  while ( tempIndexArray.length > 0 ) {
+      var index = Math.floor( Math.random() * tempIndexArray.length );
+      // Splice() deletes an element from an array and returns it as a single
+      // element array. We dereference it and push onto our array of indices.
+      my.randomizedIndices.push( tempIndexArray.splice( index , 1 ) );
+  }
     }
 
     my.postNewPics = function() {
-	// I'm gonna pop two indices so my array needs at least that many.
-	// If there is 1 or 0, we start fresh with a new randomization.
-	if ( my.randomizedIndices.length < 2 ) {
-	    my.generateRandomIndices( my.pictures.length );
-	}
+  // I'm gonna pop two indices so my array needs at least that many.
+  // If there is 1 or 0, we start fresh with a new randomization.
+  if ( my.randomizedIndices.length < 2 ) {
+      my.generateRandomIndices( my.pictures.length );
+  }
 
-	// Store left and right index
-	my.contestants = [ my.randomizedIndices.pop(), my.randomizedIndices.pop() ];
+  // Store left and right index
+  my.contestants = [ my.randomizedIndices.pop(), my.randomizedIndices.pop() ];
 
-	// If the image tags exist, update their source tags. Otherwise, create them.
-	if ( document.getElementById( "left" ) && document.getElementById( "right" ) ) {
-	    var leftImage  = document.getElementById( "left" );
-	    var rightImage = document.getElementById( "right" );
-	    leftImage.src  = ( "img/" +  my.pictures[ my.contestants[0] ].fileName );
-	    rightImage.src = ( "img/" +  my.pictures[ my.contestants[1] ].fileName );
-	} else {
-	    var leftImage =  new ImageElement( my.pictures[ my.contestants[0] ].fileName,
-					       "left");
-	    var rightImage = new ImageElement( my.pictures[ my.contestants[1] ].fileName,
-					       "right");
-	    my.anchorNode.appendChild( leftImage.element );
-	    my.anchorNode.appendChild( rightImage.element );
-	}
-	// We also need to update the chart
-	my.chartBuilder();
+  // If the image tags exist, update their source tags. Otherwise, create them.
+  if ( document.getElementById( "left" ) && document.getElementById( "right" ) ) {
+      var leftImage  = document.getElementById( "left" );
+      var rightImage = document.getElementById( "right" );
+      leftImage.src  = ( "img/" +  my.pictures[ my.contestants[0] ].fileName );
+      rightImage.src = ( "img/" +  my.pictures[ my.contestants[1] ].fileName );
+  } else {
+      var leftImage =  new ImageElement( my.pictures[ my.contestants[0] ].fileName,
+                 "left");
+      var rightImage = new ImageElement( my.pictures[ my.contestants[1] ].fileName,
+                 "right");
+      my.anchorNode.appendChild( leftImage.element );
+      my.anchorNode.appendChild( rightImage.element );
+  }
+  // We also need to update the chart
+  my.chartBuilder();
     }
 
     my.eventHandler = function() {
@@ -139,52 +127,43 @@ var VOTE_MODULE = (function() {
             console.log( " MODULE method my.click() was passes an invalid parameter" );
             return false;
         }
-	// Destroy chart and update the page with new set of pics and chart
+  // Destroy chart and update the page with new set of pics and chart
         if ( my.chart ) {
-	    my.chart.destroy()
-	}
+      my.chart.destroy()
+  }
         my.postNewPics();
     }
 
     my.chartBuilder = function() {
-	var canvasAnchor = document.getElementById( "myChart" ).getContext( "2d" );
-	var leftContestant = my.pictures[ my.contestants[0] ];
-	var rightContestant = my.pictures[ my.contestants[1] ];
-	var contestantData = {
+  var canvasAnchor = document.getElementById( "myChart" ).getContext( "2d" );
+  var leftContestant = my.pictures[ my.contestants[0] ];
+  var rightContestant = my.pictures[ my.contestants[1] ];
+  var contestantData = {
             labels: [ leftContestant.name, rightContestant.name ],
             datasets: [ { label: "Raw votes",
-			  fillColor: "rgba(220,220,220,0.5)",
-			  strokeColor: "rgba(220,220,220,0.8)",
-			  highlightFill: "rgba(220,220,220,0.75)",
-			  highlightStroke: "rgba(220,220,220,1)",
-			  // Bogus data -- use your vote counts instead
-			  data: [leftContestant.vote, rightContestant.vote] 
-			},
-		
-			{ label: "Percentage split",
-			  fillColor: "rgba(151,187,205,0.5)",
-			  strokeColor: "rgba(151,187,205,0.8)",
-			  highlightFill: "rgba(151,187,205,0.75)",
-			  highlightStroke: "rgba(151,187,205,1)",
-			  data: [ leftContestant.vote/( leftContestant.vote + rightContestant.vote ), 
-				  rightContestant.vote/( leftContestant.vote + rightContestant.vote ) ]
-			}
-		      ]
-	};
-	
-	my.chart = new Chart(canvasAnchor).Bar(contestantData);
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        // Bogus data -- use your vote counts instead
+        data: [leftContestant.vote, rightContestant.vote]
+      },
+
+      { label: "Percentage split",
+        fillColor: "rgba(151,187,205,0.5)",
+        strokeColor: "rgba(151,187,205,0.8)",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        data: [ leftContestant.vote/( leftContestant.vote + rightContestant.vote ),
+          rightContestant.vote/( leftContestant.vote + rightContestant.vote ) ]
+      }
+          ]
+  };
+  my.chart = new Chart(canvasAnchor).Bar(contestantData);
     }
-    
-    
-
-    /***********************************
-     ***** Stuff Gets Called Here ******
-     ***********************************/
-
     my.pictures.init( defaultInput );
     my.postNewPics();
     my.eventHandler();
-
-    return my;
+return my;
 
 } )();
